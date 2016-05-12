643d0c39ed651c7fe6883b9eafb463e03081d574
Abhishek Karale <k.abhishek@hotmail.co.uk>
2016-05-12 14:26:48 +0100

    Part 7 completed

--- MusicVideo/Constants.swift
+++ MusicVideo/Constants.swift
@@ -14,3 +14,10 @@ typealias JSONDictionary = [String:AnyObject]
 typealias JSONArray = Array<AnyObject>
 
 
+let WIFI = "WIFI Available"
+
+let NOACCESS = "No Internet Access"
+
+let WWAN = "Cellular Access Available"
+
+
