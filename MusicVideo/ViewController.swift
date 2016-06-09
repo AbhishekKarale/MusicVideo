1fdca3307850c892665ca2a171b5f553d926accc
Abhishek Karale <k.abhishek@hotmail.co.uk>
2016-06-09 10:52:51 +0100

    Part 11 completed

--- MusicVideo/ViewController.swift
+++ MusicVideo/ViewController.swift
@@ -8,10 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     var videos = [Videos]()
     
+    @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var displayLabel: UILabel!
     
     
@@ -26,7 +27,12 @@ class ViewController: UIViewController {
         
         //Call api-
         let api = APIManager()
-        api.loadData("https://itunes.apple.com/us/rss/topmusicvideos/limit=10/json", completion: didLoadData)
+        api.loadData("https://itunes.apple.com/us/rss/topmusicvideos/limit=50/json", completion: didLoadData)
+        
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        
         
     }
     
@@ -48,7 +54,7 @@ class ViewController: UIViewController {
             
         }
 
-        
+        tableView.reloadData()
 
         
     }
@@ -90,6 +96,43 @@ class ViewController: UIViewController {
         NSNotificationCenter.defaultCenter().removeObserver(self, name: "ReachStatusChanged", object: nil)
     }
     
+    
+    
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        
+        return 1
+        
+    }
+
+    
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return videos.count
+        
+    }
+    
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath)
+        
+        let video = videos[indexPath.row]
+        
+        cell.textLabel?.text = ("\(indexPath.row + 1)")
+        
+        cell.detailTextLabel?.text = video.vName
+        
+        return cell
+        
+        
+    }
+    
+    
+    
+
     
 
 
