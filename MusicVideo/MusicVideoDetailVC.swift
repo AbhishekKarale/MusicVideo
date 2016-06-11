b410fe715888ba499fecdff99f8a165f3673c28e
Abhishek Karale <k.abhishek@hotmail.co.uk>
2016-06-11 11:48:49 +0100

    part-18 completed

---
+++ MusicVideo/MusicVideoDetailVC.swift
@@ -0,0 +1,35 @@
+//
+//  MusicVideoDetailVC.swift
+//  MusicVideo
+//
+//  Created by Abhishek Karale on 11/06/2016.
+//  Copyright © 2016 AppMonkey. All rights reserved.
+//
+
+import UIKit
+
+class MusicVideoDetailVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
